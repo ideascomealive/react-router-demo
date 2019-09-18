434e9869ee90e3dee7b046744740d7e935ffbd7a
ideascomealive <ideascomealive@gmail.com>
2019-09-18 04:13:49 -0500

    16 using match keywork to make app dynamic

--- src/WorkDetail.js
+++ src/WorkDetail.js
@@ -2,16 +2,17 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 
-function WorkDetail() {
+function WorkDetail({ match }) {
 
 	useEffect(() => {
 		fetchDrink();
+        console.log(match);
 	}, []);
 
 	const [item, setItem] = useState({});
 
 	const fetchDrink = async () => {
-        const fetchDrink = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007
+        const fetchDrink = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${match.params.id}
 `);
         const item = await fetchDrink.json();
 
