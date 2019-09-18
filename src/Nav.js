6e49f50b95ba9bd5d5cc0943d17d89b431947b81
ideascomealive <ideascomealive@gmail.com>
2019-09-18 02:14:52 -0500

    09 inline style added for links

--- src/Nav.js
+++ src/Nav.js
@@ -2,19 +2,26 @@ import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 
+//Link component uses the to prop to power hyperlink
+
 function Nav() {
+
+  const navStyles = {
+  	color: 'white'
+  };
+
   return (
     <nav>
       <h2>Logo</h2>
       <h4>Slogan</h4>
       <ul className="nav-items">
-      	<Link to="/about">
+      	<Link style={navStyles} to="/about">
       		<li>About</li>
       	</Link>
-      	<Link to="/work">
+      	<Link style={navStyles} to="/work">
 			<li>Work</li>
       	</Link>
-      	<Link to="/testimonials">
+      	<Link style={navStyles} to="/testimonials">
 			<li>Testimonials</li>
 		</Link>
       </ul>
