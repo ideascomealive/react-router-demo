aadc2e7a20ab3e08c8f4067da3a131d8917ea2cc
ideascomealive <ideascomealive@gmail.com>
2019-09-18 02:46:13 -0500

    10 added useEffect and api fetch

--- src/Work.js
+++ src/Work.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function Work() {
+
+	useEffect(() => {
+		fetchDrinks();
+	}, []);
+
+	const fetchDrinks = async () => {
+		const data = await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka');
+		
+		const items = await data.json();
+
+		console.log(items);
+	}
   return (
     <div>
       <h1>Work</h1>
