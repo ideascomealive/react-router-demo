b29d9df5daab3f5a882939efd32fdc01ee6ebf1a
ideascomealive <ideascomealive@gmail.com>
2019-09-18 00:54:39 -0500

    01 cleanup and new pages

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function Nav() {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <h4>Slogan</h4>
+      <ul>
+      	<li>About</li>
+      	<li>Work</li>
+      	<li>Testimonials</li>
+      </ul>
+	</nav>
+  );
+}
+
+export default Nav;
